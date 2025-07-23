8626a369ba95f9ad5657f29c7db06e8585feb0e0
Sokren <remi.salandre@gmail.com>
2025-07-23 10:24:45 +0200

    correct output

--- aap/output.tf
+++ aap/output.tf
@@ -3,6 +3,6 @@
 #   value = data.http.post_request.status_code
 # }
 
- output "Status AAP" {
-   value = data.aap_job.run_job_template
+ output "Status_AAP" {
+   value = aap_job.run_job_template
  }
