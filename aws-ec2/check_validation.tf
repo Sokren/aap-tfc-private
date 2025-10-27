92e8ef4dbc1e171e9c227dc2e2a55726d3b10402
Sokren <remi.salandre@gmail.com>
2025-10-27 16:43:43 +0100

    Ajout du count

--- aws-ec2/check_validation.tf
+++ aws-ec2/check_validation.tf
@@ -1,10 +1,16 @@
+locals {
+  webserver_urls = [for ip in aws_instance.web_server[*].public_ip : "http://${ip}:8080"]
+}
+
 check "health_check_http_AAP_8080" {
-  data "http" "apache2_1_AAP" {
-    url = "http://${aws_instance.web_server.public_ip}:8080"
+  for_each = toset(local.webserver_urls)
+
+  data "http" "apache2_AAP" {
+    url = each.key
   }
 
   assert {
-    condition = data.http.apache2_1_AAP.status_code == 200
-    error_message = "${data.http.apache2_1_AAP.url} returned an unhealthy status code"
+    condition     = data.http.apache2_AAP.status_code == 200
+    error_message = "${data.http.apache2_AAP.url} returned an unhealthy status code"
   }
 }
--- aws-ec2/output.tf
+++ aws-ec2/output.tf
@@ -1,11 +1,11 @@
-output "ec2_first_addr" {
-  value = aws_instance.web_server.public_ip
+output "ec2_all_addr" {
+  value = aws_instance.web_server[*].public_ip
 }
 
-output "ec2_first_addr_http" {
-  value = "http://${aws_instance.web_server.public_ip}"
+output "ec2_all_addr_http" {
+  value = [for ip in aws_instance.web_server[*].public_ip : "http://${ip}"]
 }
 
-output "ec2_first_addr_aap_http" {
-  value = "http://${aws_instance.web_server.public_ip}:8080"
+output "ec2_all_addr_aap_http" {
+  value = [for ip in aws_instance.web_server[*].public_ip : "http://${ip}:8080"]
 }
