aea5b6c4c88d6fc2e3fbff2de925146764de072f
Sokren <remi.salandre@gmail.com>
2025-10-28 18:36:57 +0100

    test action after aap

--- aws-ec2/aap.tf
+++ aws-ec2/aap.tf
@@ -26,12 +26,12 @@ resource "aap_host" "host" {
     public_ip    = each.value.public_ip
     target_hosts = each.value.public_ip
   })
-#  lifecycle {
-#    action_trigger {
-#      events  = [after_create]
-#      actions = [action.aap_eda_eventstream_post.update]
-#    }
-#  }
+  lifecycle {
+    action_trigger {
+      events  = [after_create]
+      actions = [action.aap_eda_eventstream_post.update]
+    }
+  }
 }
 
 # TF action to run the update AWS provisioning job (after the hosts get added to AAP inventory)
@@ -39,7 +39,7 @@ action "aap_eda_eventstream_post" "update" {
   config {
     limit = "tfademo"
     template_type = "job"
-    job_template_name = "Demo Job Template"
+    job_template_name = "TFA Demo Apache Update"
     organization_name = "Default"
     event_stream_config = {
       url = var.aap_eventstream_url
--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -89,12 +89,12 @@ resource "aws_instance" "web_server" {
   tags = {
     Name = "${var.tag}-web_server-${random_pet.test.id}-${count.index}"
   }
-  lifecycle {
-    action_trigger {
-      events  = [after_create]
-      actions = [action.aap_eda_eventstream_post.create]
-    }
-  }
+#  lifecycle {
+#    action_trigger {
+#      events  = [after_create]
+#      actions = [action.aap_eda_eventstream_post.create]
+#    }
+#  }
 }
 
 action "aap_eda_eventstream_post" "create" {
