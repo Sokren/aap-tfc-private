fa5cd6cd8dfa04ea6af452762763028248de2e37
Sokren <remi.salandre@gmail.com>
2025-10-29 10:49:49 +0100

    remove inventory

--- aws-ec2/aap.tf
+++ aws-ec2/aap.tf
@@ -40,7 +40,6 @@ action "aap_eda_eventstream_post" "update" {
     limit = "98.93.9.213"
     template_type = "job"
     job_template_name = "TFA Demo Apache Update"
-    inventory_name    = aap_inventory.my_inventory.name
     organization_name = "Default"
     event_stream_config = {
       url = var.aap_eventstream_url
