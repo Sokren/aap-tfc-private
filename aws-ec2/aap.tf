5ab4c520270b8321b8039027bf6344b79ed30309
Sokren <remi.salandre@gmail.com>
2025-10-29 10:55:32 +0100

    test limit

--- aws-ec2/aap.tf
+++ aws-ec2/aap.tf
@@ -37,7 +37,7 @@ resource "aap_host" "host" {
 # TF action to run the update AWS provisioning job (after the hosts get added to AAP inventory)
 action "aap_eda_eventstream_post" "update" {
   config {
-    limit = "98.93.9.213"
+    limit = "35.170.242.108"
     template_type = "job"
     job_template_name = "TFA Demo Apache Update"
     organization_name = "Default"
