df0fe5ed2bdbdf143b72c84452e13c4e9b55f78b
Sokren <remi.salandre@gmail.com>
2025-11-12 19:04:55 +0100

    Update aap.tf

--- aws-ec2/aap.tf
+++ aws-ec2/aap.tf
@@ -29,6 +29,7 @@ resource "aap_host" "host" {
     public_ip    = each.value.public_ip
     target_hosts = each.value.public_ip
   })
+  depends_on = [time_sleep.wait_30_seconds]  
   lifecycle {
     action_trigger {
       events  = [after_create]
